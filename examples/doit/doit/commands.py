eceee5e42361d667da86576560f4232a61c2925c
Flavio Garcia <piraz@candango.org>
2020-03-09 22:23:59 -0400

    Commands module will define a list of commands.
    Refs: #2

--- examples/doit/doit/commands.py
+++ examples/doit/doit/commands.py
@@ -1,22 +1,22 @@
-from taskio.command import TaskioCommand
+from taskio.model import TaskioCommand
 
-TaskioCommand(
-    "help",
-    "Help Commands",
-    help="Show commands help.",
-    category=""
-)
-
-TaskioCommand(
-    "something",
-    "Do something",
-    help="A do something help",
-    category="do"
-)
-
-TaskioCommand(
-    "something-else",
-    "Do something else",
-    help="A do something else help",
-    category="do"
-)
+list = [
+    TaskioCommand(
+        "help1",
+        "Help Commands",
+        help="Show commands help.",
+        category_name=""
+    ),
+    TaskioCommand(
+        "something",
+        "Do something",
+        help="A do something help",
+        category_name="do"
+    ),
+    TaskioCommand(
+        "something-(else)",
+        "Do something else",
+        help="A do something else help",
+        category_name="do"
+    )
+]
