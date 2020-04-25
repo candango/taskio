c5dd72559a4a9f90b9e31d7d0dae1f162fe23109
Flavio Garcia <piraz@candango.org>
2020-04-25 10:55:41 -0400

    Added structure to start testing.
    Refs: #2

---
+++ tests/cli_test.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#
+# Copyright 2019-2020 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import (absolute_import, division, print_function,
+                        with_statement)
+
+import unittest
+import logging
+import os
+
+import sys
+# Mocking sys: https://bit.ly/2q243Tg
+try:
+    # python 3.4+ should use builtin unittest.mock not mock package
+    from unittest.mock import patch
+except ImportError:
+    from mock import patch
+
+
+class ProgramConfigTestCase(unittest.TestCase):
+    """ Case to test get_command_header
+    """
+
+    def test_header_message(self):
+        """  """
+        self.assertTrue(False)
---
+++ tests/runtests.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Copyright 2019-2020 Flavio Garcia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import (absolute_import, division, print_function,
+                        with_statement)
+
+import unittest
+from . import cli_test, config_test
+
+
+def suite():
+    testLoader = unittest.TestLoader()
+    alltests = unittest.TestSuite()
+    alltests.addTests(testLoader.loadTestsFromModule(cli_test))
+    alltests.addTests(testLoader.loadTestsFromModule(config_test))
+    return alltests
+
+
+if __name__ == "__main__":
+    runner = unittest.TextTestRunner(verbosity=3)
+    result = runner.run(suite())
+    if not result.wasSuccessful():
+        exit(2)
