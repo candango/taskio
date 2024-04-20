32a15f9613d0f2933c0ebcbf7a2bfc2897cc1ab3
Flavio Garcia <piraz@candango.org>
2024-04-20 17:59:24 -0400

    build(tests): fix import modules from tests

--- tests/runtests.py
+++ tests/runtests.py
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import unittest
-from . import cli_test, config_test
+from tests import cli_test, config_test
 
 
 def suite():
